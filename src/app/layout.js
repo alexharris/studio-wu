ae8ef639f44ac35be50e77988c41bef47f5c22d6
alex harris <alexharris6@gmail.com>
2025-09-15 20:07:11 -0700

    add homepage, add angie font

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/zyk8izc.css"></link>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,41 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        main
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-footer
-      </footer>
-    </div>
+    <>
+      <img
+        src="/homepage-bg.jpg"
+        alt="Homepage background"
+        className="homepage-bg"
+      />
+      <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <main className="flex flex-col gap-16 row-start-2 items-center">
+          <Image
+            src="/StudioWu_Logo_White_1000px.png"
+            alt="Studio Wu Logo"
+            width={500}
+            height={0}
+          />
+          <nav className="w-full">
+            <ul className="flex gap-4 text-white text-lg justify-between w-full angie uppercase">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </main>
+        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+          footer
+        </footer>
+      </div>
+    </>
   );
 }
